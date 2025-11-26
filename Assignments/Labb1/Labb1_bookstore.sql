295f0869bae5489eeaa7b44013db0cb982a88b87
Mobiasson <mikaeltobiasson@hotmail.com>
2025-11-26 16:45:35 +0100

    Added first tables, will do everything in SQL

--- Assignments/Labb1/Labb1_bookstore.sql
+++ Assignments/Labb1/Labb1_bookstore.sql
@@ -1,16 +1,34 @@
-CREATE TABLE authors (
+CREATE TABLE tbl_authors (
 id int primary key IDENTITY(1,1),
 firstname VARCHAR(15) NOT NULL,
 lastname VARCHAR(MAX) NOT NULL,
 dateofbirth DATE NOT NULL
-)
+);
 
-CREATE TABLE books (
-isbn int primary key NOT NULL,
-title VARCHAR(MAX) NOT NULL,
-language VARCHAR(15) NOT NULL,
-price int,
-publishdate DATE NOT NULL
+CREATE TABLE tbl_books (
+    isbn VARCHAR(13) PRIMARY KEY NOT NULL,
+    title VARCHAR(500) NOT NULL,
+    language VARCHAR(15) NOT NULL,
+    price DECIMAL(10,2) NOT NULL,
+    publishDate DATE NOT NULL,
+    authorId INT NOT NULL,
+    CONSTRAINT FK_books_author 
+        FOREIGN KEY (authorId) 
+        REFERENCES tbl_authors(id)
+);
 
-)
+CREATE TABLE tbl_stores (
+id int primary key IDENTITY(1,1),
+storeName VARCHAR(15) NOT NULL,
+address VARCHAR(100) NOT NULL
+);
+
+CREATE TABLE tbl_bookBalance (
+isbn VARCHAR(13) PRIMARY KEY NOT NULL,
+storeId INT NOT NULL,
+bookID INT NOT NULL
+CONSTRAINT FK_store FOREIGN KEY (storeId) REFERENCES tbl_stores(id),
+CONSTRAINT FK_isbn FOREIGN KEY (isbn) REFERENCES tbl_books(isbn)
+);
+
 
