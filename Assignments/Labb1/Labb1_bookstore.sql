ac78ae0fb853c6bdb6e7844770b97fb778deec57
Mobiasson <mikaeltobiasson@hotmail.com>
2025-12-08 09:52:59 +0100

    added .gitmodules for sub-repo. Added Labb2_Insulter to Assignments

--- Assignments/Labb1/Labb1_bookstore.sql
+++ Assignments/Labb1/Labb1_bookstore.sql
@@ -1,6 +1,6 @@
-IF OBJECT_ID('tbl_authors', 'U') IS NULL
+IF OBJECT_ID('authors', 'U') IS NULL
 BEGIN
-    CREATE TABLE tbl_authors (
+    CREATE TABLE authors (
         id INT IDENTITY(1,1) PRIMARY KEY,
         firstname VARCHAR(50) NOT NULL,
         lastname VARCHAR(100) NOT NULL,
@@ -9,9 +9,9 @@ BEGIN
     );
 END;
 
-IF NOT EXISTS(SELECT 1 FROM tbl_authors)
+IF NOT EXISTS(SELECT 1 FROM authors)
 BEGIN
-    INSERT INTO tbl_authors(firstname, lastname, dateofbirth, deathdate)VALUES
+    INSERT INTO authors(firstname, lastname, dateofbirth, deathdate)VALUES
     ('Friedrich', 'Nietzsche', '1844-10-15', '1900-08-25'),
     ('Fjodor', 'Dostoevsky', '1821-11-11', '1881-02-09'),
     ('Arthur', 'Schopenhauer', '1788-02-22', '1860-09-21'),
@@ -22,21 +22,21 @@ BEGIN
     ('Søren', 'Kierkegaard', '1813-05-05', '1855-11-11');
 END;
 
-IF OBJECT_ID('tbl_books', 'U') IS NULL
+IF OBJECT_ID('books', 'U') IS NULL
 BEGIN
-    CREATE TABLE tbl_books (
+    CREATE TABLE books (
         isbn VARCHAR(13) PRIMARY KEY NOT NULL,
         title VARCHAR(500) NOT NULL,
         language VARCHAR(15) NOT NULL,
         price DECIMAL(10,2) NOT NULL,
         publishDate DATE NOT NULL,
         authorId INT NOT NULL,
-        CONSTRAINT FK_books_author FOREIGN KEY (authorId) REFERENCES tbl_authors(id)
+        CONSTRAINT FK_books_author FOREIGN KEY (authorId) REFERENCES authors(id)
     );
 END;
 
-IF NOT EXISTS(SELECT 1 FROM tbl_books)BEGIN
-INSERT INTO tbl_books(isbn, title, language, price, publishDate, authorId)VALUES
+IF NOT EXISTS(SELECT 1 FROM books)BEGIN
+INSERT INTO books(isbn, title, language, price, publishDate, authorId)VALUES
 ('9780140441185', 'Thus Spoke Zarathustra', 'English', 169.00, '2005-11-29', 1),
 ('9780140442038', 'Beyond Good and Evil', 'English', 159.00, '2003-04-29', 1),
 ('9780140449136', 'Crime and Punishment', 'English', 229.00, '2003-02-27', 2),
@@ -55,53 +55,54 @@ END;
 
 
 
-IF OBJECT_ID('tbl_stores', 'U') IS NULL
+IF OBJECT_ID('stores', 'U') IS NULL
 BEGIN
-    CREATE TABLE tbl_stores (
+    CREATE TABLE stores (
         id INT IDENTITY(1,1) PRIMARY KEY,
         storeName VARCHAR(50) NOT NULL,
-        address VARCHAR(100) NOT NULL
+        address VARCHAR(100) NOT NULL,
+        postalCode INT NOT NULL,
+        city VARCHAR(50)
     );
 END;
 
-IF NOT EXISTS (SELECT 1 FROM tbl_stores)
+IF NOT EXISTS (SELECT 1 FROM stores)
 BEGIN
-    INSERT INTO tbl_stores (storeName, address) VALUES
-        ('Bokgrottan', 'Drottninggatan 12, Stockholm'),
-        ('Bra Böcker', 'Södra Förstadsgatan 44, Malmö'),
-        ('Filosofi Soffan','Avenyn 7, Göteborg');
+    INSERT INTO stores (storeName, address, postalCode, city) VALUES
+        ('Bokgrottan', 'Drottninggatan 12', 10475, 'Stockholm'),
+        ('Bra Böcker', 'Södra Förstadsgatan 44', 41145, 'Göteborg'),
+        ('Filosofi Soffan','Avenyn 7', 41241, 'Göteborg');
 END;
 
-IF OBJECT_ID('tbl_bookBalance', 'U') IS NULL
+IF OBJECT_ID('bookBalance', 'U') IS NULL
 BEGIN
-    CREATE TABLE tbl_bookBalance (
+    CREATE TABLE bookBalance (
         storeId INT NOT NULL,
         isbn VARCHAR(13) NOT NULL,
         amountInStock INT NOT NULL,
         CONSTRAINT PK_bookBalance PRIMARY KEY (storeId, isbn),
-        CONSTRAINT FK_bookBalance_Store FOREIGN KEY (storeId) REFERENCES tbl_stores(id),
-        CONSTRAINT FK_bookBalance_Book  FOREIGN KEY (isbn)     REFERENCES tbl_books(isbn)
+        CONSTRAINT FK_bookBalance_Store FOREIGN KEY (storeId) REFERENCES stores(id),
+        CONSTRAINT FK_bookBalance_Book  FOREIGN KEY (isbn)     REFERENCES books(isbn)
     );
 END;
 
-IF NOT EXISTS (SELECT 1 FROM tbl_bookBalance)
+IF NOT EXISTS (SELECT 1 FROM bookBalance)
 BEGIN
-    INSERT INTO tbl_bookBalance (storeId, isbn, amountInStock) VALUES
-        (1, '9780140441185', 12),
-        (1, '9780140442038', 8),
-        (1, '9780140449136', 5),
-        (1, '9780811216876', 7),
-        (2, '9780140449242', 10),
-        (2, '9780486434103', 6),
-        (2, '9780226100421', 4),
-        (3, '9781611454141', 9),
-        (3, '9780486434103', 3),
-        (3, '9780140441185', 11);
+INSERT INTO bookBalance (storeId, isbn, amountInStock) VALUES
+(1, '9780679720201', 15),
+(2, '9780141186542', 8), 
+(3, '9780679720201', 12),
+(1, '9780140444490', 20),
+(2, '9780415074578', 5), 
+(3, '9780140444490', 18),
+(1, '9780140445718', 14),
+(3, '9780691020429', 9), 
+(2, '9780811216876', 25);   
 END;
 
-IF OBJECT_ID('tbl_customers') IS NULL
+IF OBJECT_ID('customers') IS NULL
 BEGIN
-CREATE TABLE tbl_customers (
+CREATE TABLE customers (
     customerId INT IDENTITY(1,1) PRIMARY KEY,
     firstname VARCHAR(50) NOT NULL,
     lastname VARCHAR(80) NOT NULL,
@@ -111,9 +112,9 @@ CREATE TABLE tbl_customers (
 );
 END;
 
-IF NOT EXISTS (SELECT 1 FROM tbl_customers)
+IF NOT EXISTS (SELECT 1 FROM customers)
 BEGIN
-INSERT INTO tbl_customers (firstname, lastname, email, phone) VALUES
+INSERT INTO customers (firstname, lastname, email, phone) VALUES
     ('Anna','Andersson','pokemonfan92@mail.se','0701234567'),
     ('Erik','Eriksson','robloxmaster@outlook.com','0701234567'),
     ('Bob','Brandmark','minecraftking1337@gmail.com','0729876543'),
@@ -123,94 +124,93 @@ INSERT INTO tbl_customers (firstname, lastname, email, phone) VALUES
     ('Sigge','Blomma','ziggeblom@telia.com','0734455667');
 END;
 
-IF OBJECT_ID('tbl_orders') IS NULL
+IF OBJECT_ID('orders') IS NULL
 BEGIN
-CREATE TABLE tbl_orders (
+CREATE TABLE orders (
     orderId INT IDENTITY(1,1) PRIMARY KEY,
     customerId INT NOT NULL,
     orderDate DATETIME2 DEFAULT GETDATE(),
     storeId INT NOT NULL,
-    totalAmount DECIMAL(12,2) NOT NULL,
-    CONSTRAINT FK_orders_customer FOREIGN KEY (customerId) REFERENCES tbl_customers(customerId),
-    CONSTRAINT FK_orders_store FOREIGN KEY (storeId) REFERENCES tbl_stores(id)
+    CONSTRAINT FK_orders_customer FOREIGN KEY (customerId) REFERENCES customers(customerId),
+    CONSTRAINT FK_orders_store FOREIGN KEY (storeId) REFERENCES stores(id)
 );
 END;
 
-IF NOT EXISTS (SELECT 1 FROM tbl_orders)
+IF NOT EXISTS (SELECT 1 FROM orders)
 BEGIN
-INSERT INTO tbl_orders (customerId, storeId, totalAmount) VALUES
-    (1, 1, 388.00),
-    (2, 2, 219.00),
-    (3, 1, 169.00),
-    (4, 3, 448.00),
-    (5, 2, 179.00),
-    (1, 3, 159.00),
-    (6, 1, 657.00),
-    (7, 2, 189.00),
-    (3, 1, 279.00),
-    (2, 3, 328.00);
+INSERT INTO orders (customerId, storeId) VALUES
+    (1, 1),
+    (2, 2),
+    (3, 1),
+    (4, 3),
+    (5, 2),
+    (1, 3),
+    (6, 1),
+    (7, 2),
+    (3, 1),
+    (2, 3);
 END;
 
-IF OBJECT_ID('tbl_orderDetails', 'U') IS NULL
+IF OBJECT_ID('orderDetails', 'U') IS NULL
 BEGIN
-    CREATE TABLE tbl_orderDetails (
+    CREATE TABLE orderDetails (
         orderId INT NOT NULL,
         isbn VARCHAR(13) NOT NULL,
         quantity INT NOT NULL CHECK (quantity > 0),
         bookPrice DECIMAL(10, 2) NOT NULL,
         PRIMARY KEY (orderId, isbn),
-        CONSTRAINT FK_details_order FOREIGN KEY (orderId) REFERENCES tbl_orders(orderId),
-        CONSTRAINT FK_details_book FOREIGN KEY (isbn) REFERENCES tbl_books(isbn)
+        CONSTRAINT FK_details_order FOREIGN KEY (orderId) REFERENCES orders(orderId),
+        CONSTRAINT FK_details_book FOREIGN KEY (isbn) REFERENCES books(isbn)
     );
 END;
 
-IF NOT EXISTS (SELECT 1 FROM tbl_orderDetails)
+IF NOT EXISTS (SELECT 1 FROM orderDetails)
 BEGIN
-    INSERT INTO tbl_orderDetails (orderId, isbn, quantity, bookPrice) VALUES
-    (1, '9780140441185', 1, 169.00),
-    (1, '9780140442038', 1, 219.00),
-    (2, '9780140449242', 1, 219.00),
-    (3, '9780140441185', 1, 169.00),
-    (4, '9780486434103', 1, 279.00),
-    (4, '9780679720201', 1, 169.00),
-    (5, '9780679720201', 1, 179.00),
-    (6, '9780140445718', 1, 159.00),
-    (7, '9780140449136', 1, 229.00),
-    (7, '9780140449242', 1, 219.00),
-    (7, '9780140444490', 1, 209.00),
-    (8, '9780140444490', 1, 189.00),
-    (9, '9780486434103', 1, 279.00),
-    (10, '9780140441185', 1, 169.00),
-    (10, '9780140445718', 1, 159.00);
+    INSERT INTO orderDetails (orderId, isbn, quantity, bookPrice) VALUES
+    (1, '9780140441185', 49, 169.00),
+    (1, '9780140442038', 22, 219.00),
+    (2, '9780140449242', 26, 219.00),
+    (3, '9780140441185', 31, 169.00),
+    (4, '9780486434103', 59, 279.00),
+    (4, '9780679720201', 3, 169.00),
+    (5, '9780679720201', 9, 179.00),
+    (6, '9780140445718', 43, 159.00),
+    (7, '9780140449136', 32, 229.00),
+    (7, '9780140449242', 51, 219.00),
+    (7, '9780140444490', 23, 209.00),
+    (8, '9780140444490', 11, 189.00),
+    (9, '9780486434103', 14, 279.00),
+    (10, '9780140441185', 7, 169.00),
+    (10, '9780140445718', 9, 159.00);
 END;
 
 --View
-CREATE VIEW view_authorsAndTitles AS
+GO
+CREATE VIEW view_authorsPerTitle AS
 SELECT
     a.firstname + ' ' + a.lastname AS [Name],
     DATEDIFF(YEAR, a.dateofbirth, a.deathdate)
-    - CASE WHEN DATEADD(YEAR, DATEDIFF(YEAR, a.dateofbirth, a.deathdate), a.dateofbirth) > a.deathdate 
-           THEN 1 ELSE 0 END AS [Age],
+      - CASE WHEN DATEADD(YEAR, DATEDIFF(YEAR, a.dateofbirth, a.deathdate), a.dateofbirth) > a.deathdate
+             THEN 1 ELSE 0 END AS [Age],
     COUNT(b.isbn) AS [Titles],
     ISNULL(SUM(bb.amountInStock * b.price), 0) AS [Stock Value(SEK)]
-FROM tbl_authors a 
-JOIN tbl_books b          ON a.id = b.authorId 
-LEFT JOIN tbl_bookBalance bb ON b.isbn = bb.isbn
-GROUP BY 
-    a.firstname, 
-    a.lastname, 
-    a.dateofbirth, 
-    a.deathdate;
+FROM dbo.authors a
+JOIN dbo.books b
+      ON a.id = b.authorId
+LEFT JOIN dbo.bookBalance bb
+      ON b.isbn = bb.isbn
+GROUP BY a.firstname, a.lastname, a.dateofbirth, a.deathdate;
 GO
 
-SELECT * FROM view_authorsAndTitles
-ORDER BY [Name];
+SELECT * FROM view_authorsPerTitle
+ORDER BY [Titles] DESC;
 
-DROP TABLE tbl_books;
-DROP TABLE tbl_bookBalance;
-DROP TABLE tbl_stores;
-DROP TABLE tbl_authors;
-DROP TABLE tbl_orders;
-DROP TABLE tbl_orderDetails;
-DROP TABLE tbl_customers;
---REMOVE LATER
+-- Only to drop tables for testing
+--DROP VIEW IF EXISTS view_authorsPerTitle;
+--DROP TABLE IF EXISTS orderDetails;
+--DROP TABLE IF EXISTS bookBalance;
+--DROP TABLE IF EXISTS orders;
+--DROP TABLE IF EXISTS customers;
+--DROP TABLE IF EXISTS stores;
+--DROP TABLE IF EXISTS books;
+--DROP TABLE IF EXISTS authors;
